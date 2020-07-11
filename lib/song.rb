07999af94a3059534e7e75ef4ed8c6a797182bc4
kowalix007 L.K <kowalix007@gmail.com>
2020-07-11 23:52:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Song 
+  
+  attr_accessor :name, :artist
+ 
+  @@all = []
+   
+  def initialize(name)
+     @name = name
+     @@all << self
+  end 
+ 
+  def self.all
+     @@all
+  end
+  
+  def self.new_by_filename(file)
+    
+    song_ins = Song.new(file.split(" - ")[1])
+    artist = file.split(" - ").first
+    song_ins.artist_name = artist
+    song_ins
+  end
+
+  def artist_name=(name)
+     self.artist = Artist.find_or_create_by_name(name)
+
+  end 
+end 
